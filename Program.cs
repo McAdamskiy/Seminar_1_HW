929c143077a25d551681b2b5a21044797828a8a2
mcadamskiy <mcadamskiy@gmail.com>
2024-10-10 18:50:10 +0500

    task1 ended

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+    
+
+public class Answer{
+    static void CheckDivisibility(int firstNumber, int secondNumber)
+    {
+        if (firstNumber % secondNumber == 0)
+        {
+            Console.WriteLine("Divisibly");
+        }
+        else{
+            Console.WriteLine("No divisibly");
+        }
+    }
+    static public void Main(){
+        Console.WriteLine("firstNumber: ");
+        int f = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("To 0 not divise");
+        Console.WriteLine("secondNumber: ");
+        int s = Convert.ToInt32(Console.ReadLine());
+
+        CheckDivisibility(f, s);
+    }
+}
+
+
+
+
